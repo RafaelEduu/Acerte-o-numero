0c0b51d55fa7559d4ea8406a31d8f1ca8124561e
Rafael <fucahtcs@gmail.com>
2022-10-29 23:07:46 -0300

    att

--- src/index.js
+++ src/index.js
@@ -1,65 +1,46 @@
-import IMask from 'imask'
-
-var campoDeBusca = document.querySelector('#number')
-var sequenceMask = IMask(campoDeBusca, {
-  mask: Number,
-  min: 0,
-  max: 10
-})
-
-console.log(sequenceMask);
+import IMask from "imask";
 
 let number = parseInt(Math.random() * 11);
 console.log(number);
 
 let contador = 3;
+let respostaNaTela = document.querySelector("#resposta");
 
-function enviar() {
-  let respostaNaTela = document.querySelector("#resposta");
-  let enviar = document.querySelector("button");
-  
-  if (number == sequenceMask) {
+enviar.addEventListener("click", () => {
+  var campoDeBusca = document.querySelector("#number");
+  var campoDeBuscaV = campoDeBusca.value;
+  var sequenceMask = IMask(campoDeBusca, {
+    mask: Number,
+    min: 0,
+    max: 10,
+  });
+  document.getElementById('chutar').innerHTML = null
+
+  console.log(campoDeBuscaV);
+
+  if (number == campoDeBuscaV) {
+
     respostaNaTela.innerHTML = "Parabéns, você acabou de acertar o número!";
     document.getElementById("tentativas").innerHTML =
       'Para jogar novamente clique em "Novo Jogo"';
     document.getElementById("enviar").disabled = true;
     document.getElementById(
       "button"
-    ).innerHTML = `<button type=submit onclick="reiniciar()">Novo Jogo</button>`;
-  }
+    ).innerHTML = `<button type=submit id="reiniciar" onclick="reiniciar()">Novo Jogo</button>`;
+  } 
   
-  else if (sequenceMask > 10 || sequenceMask < 0) {
-    alert("Digite um número válido, entre 0 e 10");
-    document.getElementById("tentativas").innerHTML =
-      "Escreva um número novamente";
-    contador = contador;
-  }
-
-  if (sequenceMask > number) {
-    respostaNaTela.innerHTML = `Errado! O número é menor que ${sequenceMask}`;
+  if (campoDeBuscaV > number) {
+    respostaNaTela.innerHTML = `Errado! O número é menor que ${campoDeBuscaV}`;
     contador = contador - 1;
     document.getElementById(
       "tentativas"
     ).innerHTML = `Você tem mais ${contador} tentativas.`;
-  } 
-  
-  else if (sequenceMask > 10 || sequenceMask < 0) {
-    alert("Digite um número válido, entre 0 e 10");
-    document.getElementById("tentativas").innerHTML =
-      "Escreva um número novamente";
-    contador = contador;
   }
-
-  if (sequenceMask < number) {
-    respostaNaTela.innerHTML = `Errado! O número é maior que ${sequenceMask}`;
+  
+  if (campoDeBuscaV < number) {
+    respostaNaTela.innerHTML = `Errado! O número é maior que ${campoDeBuscaV}`;
     contador = contador - 1;
   } 
-  
-  else if (sequenceMask > 10 || sequenceMask < 0) {
-    alert("Digite um número válido, entre 0 e 10");
-    document.getElementById("tentativas").innerHTML =
-      "Escreva um número novamente";
-  }
 
   if (contador < 1) {
     respostaNaTela.innerHTML = `Infelizmente, você não conseguiu acertar, mas o número era o ${number}`;
@@ -67,19 +48,19 @@ function enviar() {
       "Suas chances acabaram, mas agora você pode jogar novamente, ja estou pensando em outro número aqui.";
     document.getElementById(
       "button"
-    ).innerHTML = `<button type=submit onclick="reiniciar()">Novo Jogo</button>`;
+    ).innerHTML = `<button type=submit id="reiniciar">Novo Jogo</button>`;
     document.getElementById("enviar").disabled = true;
   }
-  console.log(contador);
-}
+});
 
-
-function reiniciar() {
+let reiniciar = document.querySelector("#button");
+reiniciar.addEventListener("click", () => {
   document.getElementById("enviar").disabled = false;
   number = parseInt(Math.random() * 11);
   console.log(number);
   contador = 3;
   document.getElementById("button").innerHTML = "";
   document.getElementById("tentativas").innerHTML = "";
+  document.getElementById('chutar').innerHTML = 'Chute um novo número'
   document.getElementById("resposta").innerHTML = "Você tem 3 tentativas";
-}
+});
